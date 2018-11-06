d134ff8146f58a00a5b16947c7d045e038ae0aa4
doct0rX <mustafaj4m@gmail.com>
2018-11-06 10:27:47 +0200

    iPod

--- SoftwareConstruction_ObjectOrientedDesign/9_DesignPatterns/design-patterns-practice-projects/iPod/src/model/iPod.java
+++ SoftwareConstruction_ObjectOrientedDesign/9_DesignPatterns/design-patterns-practice-projects/iPod/src/model/iPod.java
@@ -1,10 +1,18 @@
 package model;
 
-public class iPod {
+import model.media.AbstractMedia;
+import model.media.Movie;
+import model.media.Photo;
+import model.media.Song;
+
+import java.util.*;
+
+public class iPod implements Iterable<AbstractMedia> {
 
     private String name;
-
-    // TODO: add fields here which represent this iPod's Movies, Photos, and Songs, they should be of the Collection type
+    private Set<AbstractMedia> movies = new HashSet<>();
+    private Set<AbstractMedia> photos = new HashSet<>();
+    private Collection<AbstractMedia> songs = new HashSet<>();
 
     public iPod(String name) {
         this.name = name;
@@ -13,6 +21,50 @@ public class iPod {
     // getters
     public String getName() { return name; }
 
+    /**
+     *
+     * @param movie the movie we add into the set
+     */
+    public void addMovie(Movie movie) {
+        movies.add(movie);
+    }
 
+    public void addphoto(Photo photo) {
+        photos.add(photo);
+    }
 
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+
+    @Override
+    public Iterator<AbstractMedia> iterator() {
+        return new AbstractMediaIterator();
+    }
+
+    private class AbstractMediaIterator implements Iterator<AbstractMedia> {
+
+        Iterator movieIterator = movies.iterator();
+        Iterator photoIterator = photos.iterator();
+        Iterator songIterator  =  songs.iterator();
+
+        @Override
+        public boolean hasNext() {
+            return movieIterator.hasNext() || photoIterator.hasNext() || songIterator.hasNext();
+        }
+
+        @Override
+        public AbstractMedia next() {
+            AbstractMedia media = null;
+            if (movieIterator.hasNext()) {
+                media = (Movie) movieIterator.next();
+            } else if (photoIterator.hasNext()) {
+                media = (Photo) photoIterator.next();
+            } else if (songIterator.hasNext()) {
+                media = (Song) songIterator.next();
+            }
+
+            return media;
+        }
+    }
 }
